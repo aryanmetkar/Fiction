fcfdcf49c490a04a5ab3ac75aea76817afa75f49
aryanmetkar <aryanmetkar9623@gmail.com>
2024-01-18 21:10:26 +0530

    This is Fiction App commit

---
+++ app/src/main/java/com/example/myapplication/Domain/Genres.java
@@ -0,0 +1,18 @@
+
+package com.example.myapplication.Domain; ;
+
+import java.util.List;
+
+
+public class Genres {
+
+    private List<GenresItem> genres;
+
+    public List<GenresItem> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GenresItem> genres) {
+        this.genres = genres;
+    }
+}
